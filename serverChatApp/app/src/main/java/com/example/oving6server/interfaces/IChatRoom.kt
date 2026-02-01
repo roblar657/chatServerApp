db58dbafe94d2ac0920fdd00a4f44ba7b90498c7
robelar445 <roblar757@gmail.com>
2026-02-01 14:42:21 +0100

    ...
    ...

---
+++ serverChatApp/app/src/main/java/com/example/oving6server/interfaces/IChatRoom.kt
@@ -0,0 +1,49 @@
+import com.example.oving6server.interfaces.IUser
+
+/**
+ * Et chatrom, hvor brukere kan skrive til hverandre
+ */
+interface IChatRoom {
+
+    /**
+     * Id til chat rommet
+     */
+    val id: Int
+
+    /**
+     * Legger til en bruker
+     *
+     * @param user brukeren som skal legges til.
+     */
+    fun addUser(user: IUser)
+
+    /**
+     * Fjerner en bruker
+     *
+     * @param user brukeren som skal fjernes.
+     */
+    fun removeUser(user: IUser)
+
+    /**
+     * Sjekker om en spesifikk bruker er i chat-rommet.
+     *
+     * @param user brukeren som skal sjekkes.
+     * @return true eller false, avhengig om brukeren er i rommet
+     */
+    fun contains(user: IUser): Boolean
+
+    /**
+     * Sender en melding til alle brukere i  chat-rommet.
+     *
+     * @param message meldingen som skal sendes.
+     * @param sentFrom brukeren som sender meldingen, eller `null` hvis meldingen kommer fra systemet.
+     */
+    fun broadcast(message: String, sentFrom: IUser? = null)
+
+    /**
+     * Returnerer en liste over id til alle brukere i chat-rommet
+     *
+     * @return liste over id til alle brukere i rommet.
+     */
+    fun getUserIds(): List<String>
+}
